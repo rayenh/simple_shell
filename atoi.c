377c66ca9ffb8d561a88b112e7b0fd13c7f44b43
rayenh <rayen-hmissi@outlook.fr>
2023-08-23 13:57:07 -0700

    done

--- atoi.c
+++ atoi.c
@@ -1,8 +1,20 @@
 #include "shell.h"
+
+/**
+ * interactive
+ * @inf: struct address
+ *
+ * Return: 1 if interactive  0 otherwise
+ */
+int interactive(info_t *inf)
+{
+	return (isatty(STDIN_FILENO) && inf->readfd <= 2);
+}
+
 /**
  * is_delim - checks if char is a delimeter
- * @c: char to check
- * @delim: delimeter string
+ * @c: the char to check
+ * @delim: the delimeter String
  * Return: 1 if true, 0 if false
  */
 int is_delim(char c, char *delim)
@@ -12,51 +24,13 @@ int is_delim(char c, char *delim)
 			return (1);
 	return (0);
 }
-/**
- * interactive - returns true if shell interactive 
- * @info: struct address
- * Return: 1 if interactive , 0 otherwise
- */
-int interactive(info_t *info)
-{
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
-}
-/**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- * Return: 0 if no numbers in string, converted number otherwise
- */
-int _atoi(char *s)
-{
-	int i, signe = 1, flag = 0, output;
-	unsigned int result = 0;
 
-	for (i = 0; s[i] != '\0' && flag != 2; i++)
-	{
-		if (s[i] == '-')
-			signe *= -1;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			flag = 1;
-			result *= 10;
-			result += (s[i] - '0');
-		}
-		else if (flag == 1)
-			flag = 2;
-	}
-	if (signe == -1)
-		output = -result;
-	else
-		output = result;
-
-	return (output);
-}
 /**
- * _isalpha - checks for alphabetic char
+ * _isalpha - checks for alphab char
  * @c: The char to input
- * Return: 1 if c is alphabetic, 0 otherwise
+ * Return: 1 if c is alphab, 0 otherwise
  */
+
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
@@ -65,4 +39,36 @@ int _isalpha(int c)
 		return (0);
 }
 
+/**
+ * _atoi - converts a String to an Integer
+ * @ss: the String to be converted
+ * Return: 0 if no Numbers in String, Converted Number otherwise
+ */
 
+int _atoi(char *s)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int rslt = 0;
+
+	for (i = 0; ss[i] != '\0' && flag != 2; i++)
+	{
+		if (ss[i] == '-')
+			sign *= -1;
+
+		if (ss[i] >= '0' && ss[i] <= '9')
+		{
+			flag = 1;
+			rslt *= 10;
+			rslt += (ss[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -rslt;
+	else
+		output = rslt;
+
+	return (output);
+}
